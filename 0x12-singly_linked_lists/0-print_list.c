e079a6be98668519ecd9659aee32b5a9807ebe2f
calvin sewornu <calvinsewornu123@gmail.com>
2023-10-20 18:17:07 -0700

    0 task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,12 +11,12 @@ size_t print_list(const list_t *h)
 {
 	size_t nodes_num = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		if (!h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 
 		h = h->next;
 		nodes_num++;
