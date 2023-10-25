e6f5e5435b12df6f151ca7e08e6d098d928e700e
calvin sewornu <calvinsewornu123@gmail.com>
2023-10-25 06:35:20 -0700

    almost

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+        size_t num = 0;
 
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		num++;
-	}
+        while (h != NULL)
+        {
+                printf("%d\n", h->n);
+                h = h->next;
+                num++;
+        }
 
-	return (num);
+        return (num);
 }
